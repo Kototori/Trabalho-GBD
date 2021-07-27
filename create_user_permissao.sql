ccaeb746402a3ea2866ead695993c74cfd159c54
Rafael <rafael.broedel@itixti.com.br>
2021-07-26 21:50:53 -0300

    inicio scipt criacao de usuarios

---
+++ create_user_permissao.sql
@@ -0,0 +1,9 @@
+CREATE USER 'DBA'@'localhost' IDENTIFIED BY 'dba.123';
+
+GRANT ALL PRIVILEGES ON *.* TO 'DBA'@'localhost';
+
+CREATE USER 'analisa'@'localhost' IDENTIFIED BY 'analisa.123';
+
+CREATE USER 'usuario1'@'localhost' IDENTIFIED BY 'usuario1.123';
+
+CREATE USER 'usuario2'@'localhost' IDENTIFIED BY 'usuario2.123';
