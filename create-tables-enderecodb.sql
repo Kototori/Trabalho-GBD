50d47e36a0ed9bce66130aa96f0c4af628a75cd8
Dener Canal <dener.canal@itixti.com.br>
2021-07-27 00:04:08 -0300

    add db name

--- create-tables-enderecodb.sql
+++ create-tables-enderecodb.sql
@@ -1,36 +1,36 @@
-CREATE TABLE pais (
+CREATE TABLE enderecodb.pais (
 	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	nome varchar(255)
 );
 
-CREATE TABLE unidade_federativa (
+CREATE TABLE enderecodb.unidade_federativa (
 	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	nome varchar(255),
 	sigla varchar(2),
 	pais_id int,
-	CONSTRAINT FK_UF_Pais FOREIGN KEY (pais_id) REFERENCES pais(id)
+	CONSTRAINT FK_UF_Pais FOREIGN KEY (pais_id) REFERENCES enderecodb.pais(id)
 );
 
-CREATE TABLE cidade (
+CREATE TABLE enderecodb.cidade (
 	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	nome varchar(255),
 	uf_id int,
-	CONSTRAINT FK_Cidade_UF FOREIGN KEY (uf_id) REFERENCES unidade_federativa(id)
+	CONSTRAINT FK_Cidade_UF FOREIGN KEY (uf_id) REFERENCES enderecodb.unidade_federativa(id)
 );
 
-CREATE TABLE bairro (
+CREATE TABLE enderecodb.bairro (
 	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	nome varchar(255),
 	cep varchar(255),
 	cidade_id int,
-	CONSTRAINT FK_Bairo_Cidade FOREIGN KEY (cidade_id) REFERENCES cidade(id)
+	CONSTRAINT FK_Bairo_Cidade FOREIGN KEY (cidade_id) REFERENCES enderecodb.cidade(id)
 );
 
-CREATE TABLE endereco (
+CREATE TABLE enderecodb.endereco (
 	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	logradouro varchar(255),
 	numero varchar(255),
 	complemento varchar(255),
 	bairro_id int,
-	CONSTRAINT FK_Endereco_Bairro FOREIGN KEY (bairro_id) REFERENCES bairro(id)
+	CONSTRAINT FK_Endereco_Bairro FOREIGN KEY (bairro_id) REFERENCES enderecodb.bairro(id)
 );
