39f1eee882b7b67d1a49667f5c1aac8b5d6c68a5
Rafael <rafael.broedel@itixti.com.br>
2021-08-15 12:06:44 -0300

    ajuste nos grants

--- create_user_permissao.sql
+++ create_user_permissao.sql
@@ -1,22 +1,21 @@
 # EXECUTAR COMO ROOT
-CREATE USER 'DBA'@'%' IDENTIFIED BY 'dba.123';
+CREATE USER 'dba'@'%' IDENTIFIED BY 'dba.123';
 
-CREATE USER 'DBA'@'localhost' IDENTIFIED BY 'dba.123';
-
-GRANT ALL ON *.* TO 'DBA'@'%';
-
-GRANT GRANT OPTION ON *.* TO 'DBA'@'%';
-
-GRANT ALL ON *.* TO 'DBA'@'localhost';
-
-GRANT GRANT OPTION ON *.* TO 'DBA'@'localhost';
+GRANT ALL ON *.* TO 'dba'@'%' WITH GRANT OPTION;
 
 # EXECUTAR COMO DBA
 CREATE USER 'analista'@'%' IDENTIFIED BY 'analista.123';
 
-# EXECUTAR COMO DBA SOMENTA APOS CRIACAO DOS DATABASES
-GRANT CREATE, SELECT, SUPER, ALTER, TRIGGER, INSERT, REFERENCES, CREATE ROUTINE, ALTER ROUTINE, TRIGGER ON *.* TO 'analista'@'%';
+GRANT CREATE, SELECT, SUPER, ALTER, TRIGGER, INSERT, UPDATE, REFERENCES, CREATE ROUTINE, ALTER ROUTINE, TRIGGER ON *.* TO 'analista'@'%';
 
 CREATE USER 'usuario1'@'%' IDENTIFIED BY 'usuario1.123';
+
+GRANT UPDATE ON rhdb.historico_carreira TO 'usuario1'@'%';
+
+#GRANTS PARA QUEBRAR A BASE
+GRANT SELECT ON rhdb.pessoa_fisica TO 'usuario1'@'%';
+GRANT SELECT ON rhdb.escolaridade TO 'usuario1'@'%';
+GRANT SELECT ON rhdb.salario_base TO 'usuario1'@'%';
+GRANT INSERT ON rhdb.historico_carreira TO 'usuario1'@'%';
 
 CREATE USER 'usuario2'@'%' IDENTIFIED BY 'usuario2.123';
